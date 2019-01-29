f2980c3e13fa9135d8670b28644d78979f0921c4
Kostya <koortttt@gmail.com>
2019-01-29 21:57:08 +0200

    need do smth at home

--- script.js
+++ script.js
@@ -1,1 +1,26 @@
-alert('asdasdas');
+// item.innerHTML += '<h1>aadasd</h1>'
+// console.log(item.innerHTML)
+
+
+// item.style.color = 'orange';
+// item.style.fontSize = '123px';
+// item.style.textAlign = 'left';
+
+// function toggleClassFirstItem() {
+//     let element = document.getElementById("list");
+//     element.classList.toggle("active");
+// }
+// toggleClassFirstItem();
+
+// function myFunction() {
+//     let element = document.getElementById("element");
+//     element.classList.toggle("active");
+// }
+
+function toggleClassFirstItem() {
+    const firstItem = document.querySelectorAll('.list.list-item')[0];
+    firstItem.classList.toggle('active');
+
+}
+
+toggleClassFirstItem();
