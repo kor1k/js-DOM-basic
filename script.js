42996ae064964179400695bbf9a4afa38ad03c18
korik <koortttt@gmail.com>
2019-02-05 20:25:12 +0200

    events in JS click, eventlistener, entire documents clicks.

--- script.js
+++ script.js
@@ -17,11 +17,45 @@
 //     element.classList.toggle("active");
 // }
 
+//
+// function toggleClassFirstItem() {
+//     const firstItem = document.querySelectorAll('.list .list-item')[0];
+//     firstItem.classList.toggle('active');
+//
+// }
+// toggleClassFirstItem();
 
-function toggleClassFirstItem() {
-    const firstItem = document.querySelectorAll('.list .list-item')[0];
-    firstItem.classList.toggle('active');
+                                                // 05.02
 
-}
+//
+const button = document.getElementsByTagName('button')[0];
+// function buttonClick() {
+//     console.log('clicked')
+// }
+//
+// console.dir(button)
+//
+// button.onclick = function () {
+//     console.log('button clicked again')
+// }
+//
 
-toggleClassFirstItem();
+document.addEventListener('click', function (event) {
+    // console.dir(event);
+    const allButtons = document.getElementsByTagName('button');
+    let isButton = false;
+    for (let i = 0; i < allButtons.length; i++) {
+        if (event.target === allButtons[i]) {
+            isButton = true;
+            break;
+        }
+    }
+    if (isButton){
+        console.log('clicked on button')
+    } else {
+        console.log('clicked on document page');
+    }
+});
+
+//TODO
+// `eventListener, .dir, onclick in html, onclick in js, event`
